816937c1410de0c7099591b8a422a8afc5ad7def
Kubiche <juanquy25@gmail.com>
2022-05-24 15:01:17 -0400

    formating cleanup

--- src/inboundMessages.cpp
+++ src/inboundMessages.cpp
@@ -4,7 +4,7 @@
 // Message handler required by simpit.
 void messageHandler(uint8_t messageType, uint8_t msg[], uint8_t msgSize) {
   switch(messageType) {
-  case SF_STAGE_MESSAGE: //Current stage solid fuel
+    case SF_STAGE_MESSAGE: //Current stage solid fuel
       // Checking if the message is the size we expect is a very basic
       // way to confirm if the message was received properly.
       if (msgSize == sizeof(resourceMessage)){
@@ -19,8 +19,8 @@ void messageHandler(uint8_t messageType, uint8_t msg[], uint8_t msgSize) {
         show_in_bar_4(map(mySStageFuel.available, 0, mySStageFuel.total, 1, 10));
         }      
       }
-    break;
-  case LF_STAGE_MESSAGE: //Current stage liquid fuel
+      break;
+    case LF_STAGE_MESSAGE: //Current stage liquid fuel
       if (msgSize == sizeof(resourceMessage)){
         resourceMessage myLStageFuel;
         myLStageFuel = parseResource(msg);
@@ -31,8 +31,8 @@ void messageHandler(uint8_t messageType, uint8_t msg[], uint8_t msgSize) {
           show_in_bar_3(map(myLStageFuel.available, 0, myLStageFuel.total, 1, 10));
         }
       }
-    break;
-  case MONO_MESSAGE: //Vessel total Monopropellant fuel
+      break;
+    case MONO_MESSAGE: //Vessel total Monopropellant fuel
       if (msgSize == sizeof(resourceMessage)){
         resourceMessage myMonoFuel;
         myMonoFuel = parseResource(msg);
@@ -43,8 +43,8 @@ void messageHandler(uint8_t messageType, uint8_t msg[], uint8_t msgSize) {
           show_in_bar_2(map(myMonoFuel.available, 0, myMonoFuel.total, 1, 10));
         }
       }
-    break;
-  case ELECTRIC_MESSAGE: //Vessel total Battery charge
+      break;
+    case ELECTRIC_MESSAGE: //Vessel total Battery charge
       if (msgSize == sizeof(resourceMessage)){
         resourceMessage myBatteryLevel;
         myBatteryLevel = parseResource(msg);
@@ -55,8 +55,8 @@ void messageHandler(uint8_t messageType, uint8_t msg[], uint8_t msgSize) {
         show_in_bar_1(map(myBatteryLevel.available, 0, myBatteryLevel.total, 1, 10));
         }
       }
-    break;
-  case SF_MESSAGE:  ////Vessel total Solid fuel
+      break;
+    case SF_MESSAGE:  ////Vessel total Solid fuel
       if (msgSize == sizeof(resourceMessage)){
         resourceMessage mySFuel;
         mySFuel = parseResource(msg);
@@ -67,26 +67,30 @@ void messageHandler(uint8_t messageType, uint8_t msg[], uint8_t msgSize) {
           show_in_bar_4(map(mySFuel.available, 0, mySFuel.total, 1, 10));
         }
       }
-    break;  
-  case LF_MESSAGE:  ////Vessel total Liquid fuel
-      if (msgSize == sizeof(resourceMessage)){
+      break;  
+    case LF_MESSAGE:  ////Vessel total Liquid fuel
+      if (msgSize == sizeof(resourceMessage))
+      {
         resourceMessage myLFuel;
         myLFuel = parseResource(msg);
-        if (myLFuel.available == 0){
-          show_in_bar_2(0);
-        }
-        else {
-          show_in_bar_2(map(myLFuel.available, 0, myLFuel.total, 1, 10));
-        }
+      if (myLFuel.available == 0)
+      {
+        show_in_bar_2(0);
       }
-    break;
-  case ACTIONSTATUS_MESSAGE:  ////Vessel total Solid fuel
-    if (msg[0] & SAS_ACTION){
-      setLed(7, 1, true);      
+      else {
+        show_in_bar_2(map(myLFuel.available, 0, myLFuel.total, 1, 10));
+      }
     }
-    else{
-      setLed(7,1,false);
-    } 
+      break;
+    case ACTIONSTATUS_MESSAGE:  ////Vessel total Solid fuel
+      if (msg[0] & SAS_ACTION)
+      {
+        setLed(7, 1, true);      
+      }
+      else
+      {
+        setLed(7,1,false);
+      } 
     break;
   }
 
