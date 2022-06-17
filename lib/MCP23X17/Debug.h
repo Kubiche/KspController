1dc7ce3f97bbedb1d61d31992a7faddcb59ff75f
Kubiche <juanquy25@gmail.com>
2022-06-17 15:28:40 -0400

    debug changes

--- lib/MCP23X17/Debug.h
+++ lib/MCP23X17/Debug.h
@@ -3,16 +3,9 @@
 #define DEBUG 0
 
 #if DEBUG == 1
-<<<<<<< HEAD
 #define debug(x) Serial.print(x)
 #define debugln(x, y) Serial.println(x, y)
 #else
 #define debug(x)
-=======
-#define debug(x, y) Serial.print(x, y)
-#define debugln(x, y) Serial.println(x, y)
-#else
-#define debug(x, y)
->>>>>>> c978ae64cd07af11917ccc2997560cb28d1c5671
 #define debugln(x, y)
 #endif                                            
