a5a9df7cb0886fb77332bb865f5edeae66252b83
Kubiche <juanquy25@gmail.com>
2022-08-10 15:20:24 -0400

    Cleanup

--- src/Defines.h
+++ src/Defines.h
@@ -2,7 +2,8 @@
 
 // The SPI Slave Select pin for the led controller
 #define LED_CS 10
-#define Bar_Device 0
+#define Controller_v2 0
+#define Controller_v3 1
 
 
 #define ANALOG_CHECK_INTERVAL 5 // Analog read interval to prevent USB saturation.
